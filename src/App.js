8928f592bd22cabea806a96126e7841fcdac7a1a
Sazzad Hossain <sharifh530@gmail.com>
2020-08-04 23:13:55 +0600

    Using axios fetched data from tmdb api, showed it to homepage using Row component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+
+import Row from "./Row";
+import request from "./request";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
+      <Row title="TRENDING NOW" fetchUrl={request.fetchTrending} />
+      <Row />
+      <Row />
+      <Row />
     </div>
   );
 }
