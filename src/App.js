be3157374e929b3fdcf6cc1f2bc7c11fa9f0be00
Sazzad Hossain <sharifh530@gmail.com>
2020-08-04 23:18:58 +0600

    Made the catagories of movies using Row component

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ function App() {
       <h1>Hello World</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} />
       <Row title="TRENDING NOW" fetchUrl={request.fetchTrending} />
-      <Row />
-      <Row />
-      <Row />
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries} />
     </div>
   );
 }
--- src/Row.jsx
+++ src/Row.jsx
@@ -28,6 +28,7 @@ function Row({ title, fetchUrl }) {
       <div className="row__posters">
         {movies.map((movie) => (
           <img
+            key={movie.id}
             className="row__poster"
             src={`${base_url}${movie.poster_path}`}
             alt={movie.name}
